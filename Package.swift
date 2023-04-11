ee5f4caa157d709c901c438119ac3e2fa4857d70
RickStreet <Rick_1@me.com>
2023-04-11 11:58:54 -0700

    Changed from StringKit to NSStringKit

--- Package.swift
+++ Package.swift
@@ -13,18 +13,16 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/RickStreet/DoubleKit.git", from: "1.0.6"),
-        .package(url: "https://github.com/RickStreet/StringKit.git", from: "1.0.25")
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/RickStreet/NSStringKit.git", from: "1.0.03")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XYPlotKit",
-            dependencies: ["StringKit", "DoubleKit"]),
+            dependencies: ["DoubleKit", "NSStringKit"]),
         .testTarget(
             name: "XYPlotKitTests",
-            dependencies: ["XYPlotKit", "StringKit", "DoubleKit"]),
+            dependencies: ["XYPlotKit", "DoubleKit", "NSStringKit"]),
     ]
 )

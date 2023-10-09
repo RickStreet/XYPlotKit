091488fd9b3abab6a791eeea032d7e72270d7895
RickStreet <Rick_1@me.com>
2023-10-09 16:57:18 -0700

    Moved AxisSpacing() to its own package.

--- Package.swift
+++ Package.swift
@@ -13,16 +13,18 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/RickStreet/DoubleKit.git", from: "1.0.6"),
-        .package(url: "https://github.com/RickStreet/NSStringKit.git", from: "1.0.08")
+        .package(url: "https://github.com/RickStreet/NSStringKit.git", from: "1.0.08"),
+        .package(url: "https://github.com/RickStreet/AxisSpacing.git", from: "1.0.0")
+
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XYPlotKit",
-            dependencies: ["DoubleKit", "NSStringKit"]),
+            dependencies: ["DoubleKit", "NSStringKit", "AxisSpacing"]),
         .testTarget(
             name: "XYPlotKitTests",
-            dependencies: ["XYPlotKit", "DoubleKit", "NSStringKit"]),
+            dependencies: ["XYPlotKit", "DoubleKit", "NSStringKit", "AxisSpacing"]),
     ]
 )
